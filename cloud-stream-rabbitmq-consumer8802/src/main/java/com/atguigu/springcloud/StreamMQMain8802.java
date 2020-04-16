b132c60bf532cca0e944fa3609e63ac93d78358d
stefanieChen666 <m17775787302@163.com>
2020-04-16 17:08:02 +0800

    Stream消息驱动

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/StreamMQMain8802.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: springcloud2020
+ * @author: Miss.CHEN
+ * @create: 2020-04-16 16:17
+ **/
+@SpringBootApplication
+public class StreamMQMain8802
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StreamMQMain8802.class,args);
+    }
+}
+
